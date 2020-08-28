909e0a4160999223c568b88b8f0674c56d980fef
wfchiang <weifan.wf@gmail.com>
2020-08-27 20:34:16 -0400

    fix the unit testing

--- simple-db/src/test.SpreadsheetCell.js
+++ simple-db/src/test.SpreadsheetCell.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { configure, shallow } from 'enzyme';
 import { expect } from 'chai';
 import Adapter from 'enzyme-adapter-react-16'
+import sinon from 'sinon'
 configure({ adapter: new Adapter() });
 
 import SpreadsheetCell from "./SpreadsheetCell"
@@ -10,14 +11,35 @@ import SpreadsheetCell from "./SpreadsheetCell"
 describe("Tests for SpreadsheetCell", () => {
     it("Always Pass!", () => {}); 
 
-    it("Create a shadow element", () => {
+    it("isFixed is true", () => {
         const ssCell = shallow(
             <SpreadsheetCell
                 cellKey="ss-cell-101-xyz" 
                 isFixed={true} 
                 activeData="123" />
         ); 
-        
         expect(ssCell.text()).to.eq("123"); 
+    }); 
+
+    it("isFixed is false. Initial state", () => {
+        const spyHandleOnClickText = sinon.spy(SpreadsheetCell.prototype, 'handleOnClickText'); 
+        const ssCell = shallow(
+            <SpreadsheetCell
+                cellKey="ss-cell-101-xyz" 
+                isFixed={false} 
+                activeData="123" 
+                backupData="123" />
+        );
+        expect(ssCell.text()).to.eq("123");
+        expect(ssCell.instance().state.inEdit).to.eq(false);  
+
+        // click it! 
+        ssCell.simulate('click');
+        ssCell.setProps(ssCell.instance().state); 
+        expect(spyHandleOnClickText.callCount).to.eq(1); 
+        expect(ssCell.instance().state.inEdit).to.eq(true);     
+        expect(ssCell.find('input').exists()).to.be.eq(true); 
+        expect(ssCell.find('input').length).to.be.eq(1); 
+        expect(ssCell.find('input').prop('value')).to.be.eq('123'); 
     }); 
 }); 
